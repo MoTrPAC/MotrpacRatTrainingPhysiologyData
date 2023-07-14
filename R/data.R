1be33f8b2d3c8c95b6e9321923d665be33bb0182
TylerSagendorf <tjsagendorf@gmail.com>
2023-07-14 16:50:48 -0700

    Update documentation
    Minor updates to include basic documentation for some datasets.

--- R/data.R
+++ R/data.R
@@ -1,9 +1,51 @@
 #' @title All physiology data
 #'
 #' @description All physiology measures combined into a single
-#'   \code{data.table}.
+#'   \code{data.frame}.
 #'
 #' @usage PHYSIO
 #'
 #' @keywords datasets
 "PHYSIO"
+
+#' @title Plasma clinical analytes
+#'
+#' @description A \code{data.frame} containing common clinical analytes measured
+#'   in plasma.
+#'
+#' @usage ANALYTES
+#'
+#' @keywords datasets
+"ANALYTES"
+
+#' @title Estimated marginal means object for each plasma clinical analyte
+#'
+#' @description A list of \code{emmGrid} objects, one for each clinical analyte.
+#'
+#' @usage ANALYTES_EMM
+#'
+#' @keywords datasets
+"ANALYTES_EMM"
+
+#' @title Results of statistical analysis of plasma clinical analytes
+#'
+#' @description A \code{data.frame} containing analysis results for common
+#'   clinical analytes measured in plasma.
+#'
+#' @usage ANALYTES_STATS
+#'
+#' @keywords datasets
+"ANALYTES_STATS"
+
+
+#' @title NMR body composition
+#'
+#' @description A \code{data.frame} containing measures of body composition
+#'   (total body mass, body fat, % body fat, lean mass, and % lean mass) both
+#'   pre- and post-training.
+#'
+#' @usage NMR
+#'
+#' @keywords datasets
+"NMR"
+
