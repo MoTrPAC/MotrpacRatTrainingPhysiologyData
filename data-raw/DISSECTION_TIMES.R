1a1f4b06e6fcf1c3fee7b62640b0e832587d62d0
TylerSagendorf <tjsagendorf@gmail.com>
2023-08-14 10:34:32 -0700

    Update DISSECTION_TIMES
    Add missing files to data-raw/ and correct dissection order.

--- data-raw/DISSECTION_TIMES.R
+++ data-raw/DISSECTION_TIMES.R
@@ -1,8 +1,6 @@
 library(MotrpacRatTrainingPhysiologyData)
 library(dplyr)
 library(lubridate)
-library(ggplot2)
-library(scales)
 
 ## 6-month data ================================================================
 sp6 <- read.csv("./data-raw/DMAQC_Transfer_PASS_1B.6M_1.02_DS_MoTrPAC.PASS_Animal.Specimen.Processing.csv") %>%
@@ -15,7 +13,7 @@ sc6 <- read.csv("./data-raw/DMAQC_Transfer_PASS_1B.6M_1.02_DS_MoTrPAC.PASS_Anima
 
 x6 <- full_join(sp6, sc6, by = "pid")
 
-unique(x6$aliquotdescription)
+unique(x6$aliquotdescription) # 25 descriptions, not all relevant
 
 ## 18-month data ===============================================================
 sp18 <- read.csv("./data-raw/DMAQC_Transfer_PASS_1B.18M_1.00_DS_MoTrPAC.PASS_Animal.Specimen.Processing.csv") #%>%
@@ -36,21 +34,21 @@ dissection_order <- c(
   "Liver" = "Liver",
   "Vena Cava" = "Vena Cava",
   "Lung" = "Lung",
+  "Heart" = "Heart",
   # Decapitation
   # Brain dissection
-  "Cortex Right" = "Cortex (right)",
-  "Cortex Left" = "Cortex (left)",
   "Hypothalamus" = "Hypothalamus",
   "Hippocampus" = "Hippocampus",
+  "Cortex Right" = "Cortex (right)",
+  "Cortex Left" = "Cortex (left)",
   # Dissection of remaining tissues
-  "Heart" = "Heart",
-  "Aorta" = "Aorta",
   "Kidney" = "Kidney",
   "Adrenals (both)" = "Adrenals (both)",
   "Spleen" = "Spleen",
   "Brown Adipose" = "Brown Adipose",
-  "Small Intestine" = "Jejunum",
+  "Aorta" = "Aorta",
   "Colon" = "Colon",
+  "Small Intestine" = "Jejunum",
   "Feces" = "Feces",
   "Testes (left)" = "Testes (left)",
   "Ovaries (both)" = "Ovaries (both)",
@@ -59,7 +57,10 @@ dissection_order <- c(
   "Femur" = "Femur"
 )
 
-DISSECTION_TIMES <- plyr::rbind.fill(x6, x18) %>%
+# Subset to columns in common
+x6 <- x6[, colnames(x18)]
+
+DISSECTION_TIMES <- rbind(x6, x18) %>%
   # select(pid:aliquotdescription, t_collection, t_freeze, t_death) %>%
   select(pid, techid, sampletypedescription,
          aliquotdescription, t_collection, t_freeze, t_death) %>%
@@ -84,7 +85,7 @@ DISSECTION_TIMES <- plyr::rbind.fill(x6, x18) %>%
 
          across(c(t_collection, t_death, t_freeze),
                 ~ as.integer(.x - pmin(t_collection, t_death, t_freeze))),
-         collect_before_death = aliquotdescription %in% dissection_order[1:8],
+         collect_before_death = aliquotdescription %in% dissection_order[1:9],
          freeze_after_death = ifelse(!collect_before_death,
                                      t_freeze >= t_death, NA),
          freeze_after_collect = t_freeze >= t_collection) %>%
@@ -92,7 +93,8 @@ DISSECTION_TIMES <- plyr::rbind.fill(x6, x18) %>%
   filter(!aliquotdescription %in% c("Femur", "Soleus", "Plantaris"))
 
 # Fix data entry errors (swap t_freeze and t_collection for a few rows)
-idx <- which(with(DISSECTION_TIMES, !freeze_after_death | !freeze_after_collect))
+idx <- which(with(DISSECTION_TIMES,
+                  !freeze_after_death | !freeze_after_collect))
 DISSECTION_TIMES[idx, c("t_freeze", "t_collection")] <-
   DISSECTION_TIMES[idx, c("t_collection", "t_freeze")]
 
@@ -107,22 +109,15 @@ DISSECTION_TIMES <- DISSECTION_TIMES %>%
   filter(!is.na(sex), !is.na(age)) %>%
   relocate(t_freeze, .after = t_death)
 
-152 * 19 + 23 * 130 # 5878 rows
-# DISSECTION_TIMES has 6035 rows before removing those with missing sex or age
-# and 5759 rows after.
-
-## Used to identify data entry errors
+## Used to identify data entry errors (should be an empty table)
 # DISSECTION_TIMES %>%
 #   filter(!freeze_after_death | !freeze_after_collect | t_diff < 0) %>%
 #   View()
 
+## Data-entry errors (fixed)
 # 11464089 liver t_freeze 49 min, not 48
 # 11621443 cortex t_freeze 37 min, not 36
 # 12626880 hippocampus and hypothalamus and kidney 12 hr --> 13 hr
-
-
-# Color-code letters or boxes? panels for dissector
-# Separate death from under anesthesia
 
 # Save
 usethis::use_data(DISSECTION_TIMES, internal = FALSE, overwrite = TRUE,
