19229c382863a0d73ed7cf6fc0ae9d2a679e051a
TylerSagendorf <tjsagendorf@gmail.com>
2024-01-18 17:37:51 -0600

    Update global variables

--- R/globals.R
+++ R/globals.R
@@ -2,5 +2,5 @@ utils::globalVariables(
   c("group", "lower.CL", "asymp.LCL", "upper.CL", "asymp.UCL", "response_mean",
     "contrast", "type", "contrast", "group1", "group2", "p.value",
     "p.adj", "pid", "age", "sex", "group_num", "color", "group_offset",
-    "y_position")
+    "y_position", "aliquotdescription", "t_diff", "label", "y", "width")
   )
